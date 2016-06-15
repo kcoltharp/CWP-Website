79b2413037e1d54aaf43b2d9b15e8235f363d23d
Kenny Coltharp <kcoltharp@gmail.com>
2016-06-15 17:45:40 -0400

    - Inserted the PHP Login script into the section "aside" - Functionality still needs checked Signed-off-by:Kenny Coltharp <kcoltharp@gmail.com>

--- includes/aside.php
+++ includes/aside.php
@@ -1,9 +1,13 @@
 <aside>
 	<?php
-	if(logged_in() === true){
-		require_once 'includes/widgets/loggedin.php';
-	} else{
-		require_once 'includes/widgets/login.php';
-	}
+	/*
+	  if(logged_in() === true){
+	  require_once 'includes/widgets/loggedin.php';
+	  } else{
+	  require_once 'includes/widgets/login.php';
+	  }
+	 *
+	 */
+	require_once 'C:/xampp/htdocs/cwp-test/phpLogin/index.php';
 	?>
 </aside>
--- includes/head.php
+++ includes/head.php
@@ -6,16 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keywords" content="SC CWP, South Carolina Concealed, Concealed,South Carolina Weapon, Weapon,South Carolina Permit, Permit, Concealed Permit, Concealed Weapon, Concealed Weapon Permit, South Carolina CWP, South Carolina Concealed Weapon Permit" />
 	<!-- CDN Requests -->
-
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-	<!--
-	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-	-->
 	<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.min.js"></script>
 	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
-	<!-- Google Fonts
-	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-	-->
+	<!-- Google Fonts -->
+	<!-- <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">	-->
 	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.css"rel="stylesheet" type="text/css">
 	<link href='https://fonts.googleapis.com/css?family=Faster+One' rel='stylesheet' type='text/css'>
 
@@ -25,9 +20,4 @@
 	<link rel="stylesheet" href="styles/main.css">
 	<link rel="stylesheet" href="styles/screen.css">
 	<script src="js/main.js"></script>
-	<script>
-		$(function(){
-			$("date").innerHTML = Date();
-		});
-	</script>
 </head>
